1dadd00066d6b7d8b9be102f5df316e8b6bae2a3
jesus <camerojesus@gmail.com>
2025-01-02 07:57:29 -0400

    Guardar consulta en .row y agregar variables de entorno .env

--- conexion-mariadb.js
+++ conexion-mariadb.js
@@ -1,11 +1,15 @@
 import mariadb from 'mariadb';
+import fs from 'fs';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const config={
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'gmaacc2971',
-    database: 'dialca',
-    port: 3310
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    port: Number(process.env.DB_PORT)
 }
 
 async function CargarRegistros(cNombreTabla,nLimiteRegistros) {
@@ -14,7 +18,15 @@ async function CargarRegistros(cNombreTabla,nLimiteRegistros) {
         connection= await mariadb.createConnection(config);
         const row= await connection.query(`select * from ${cNombreTabla}  limit ${nLimiteRegistros} `);
         console.log("Filas de la tabla: ",cNombreTabla);
-        console.table(row)
+        console.table(row);
+
+        // Guardar row en un archivo de texto
+        try {
+            fs.writeFileSync(`${cNombreTabla}.row`, JSON.stringify(row, null, 2));
+            console.log(`Datos guardados en ${cNombreTabla}.row`);
+        } catch (writeError) {
+            console.log(`Error al guardar los datos en ${cNombreTabla}.row: `, writeError);
+        }
 
     } catch(error) {
         console.log("Error al cargar los clientes: ",error)
@@ -25,4 +37,4 @@ async function CargarRegistros(cNombreTabla,nLimiteRegistros) {
     }
 }
 
-CargarRegistros("cliente",20); 
+CargarRegistros("proveedor",20);
